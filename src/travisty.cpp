730e20f5bde06cf0d5b716aeb08758ce9045462d
Andy Sawyer <git@pureabstract.org>
2014-09-01 21:12:09 +0100

    Add some banner lines

--- src/travisty.cpp
+++ src/travisty.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+    std::cout << "====================\n";
 #ifdef __GNUC__
     std::cout << "__GNUC__\n";
 #endif
@@ -9,5 +10,6 @@ int main()
     std::cout << "__clang__\n";
 #endif
     std::cout << "__cplusplus:" << __cplusplus << '\n';
+    std::cout << "====================\n";
     return 0;
 }
